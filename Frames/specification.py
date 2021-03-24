f52d56769c818f10494a86e6a11643385643d538
ape <sasha.penzev76@gmail.com>
2021-03-24 21:35:17 +0200

    added price calculation and make spinbox editable.

--- Frames/specification.py
+++ Frames/specification.py
@@ -1,5 +1,7 @@
+import decimal
 import tkinter as tk
 import tkinter.ttk as ttk
+from decimal import Decimal, ROUND_HALF_UP
 
 from Utills.utills import save_spec, price_from_str
 from Utills.autocomplete_entry_words import AutocompleteEntryWords
@@ -8,6 +10,8 @@ from Utills.autocomplete_entry_words import AutocompleteEntryWords
 class Specification(tk.LabelFrame):
     def __init__(self, **kw):
         super().__init__(**kw)
+        self.MIN_NUMBER_POSITION = 1
+        self.MAX_NUMBER_POSITION = 30
         self.entries = []
         self.measurement_autocomplete = []
 
@@ -15,9 +19,14 @@ class Specification(tk.LabelFrame):
         self.count_spec_position_frame.grid()
         self.count_spec_position_label = tk.Label(self.count_spec_position_frame, text='Кількість позицій', width=14)
         self.count_spec_position_label.grid(row=1, column=0)
-        self.count_spec_position = ttk.Spinbox(self.count_spec_position_frame, state="readonly", from_=1, to=30)
+        self.count_spec_position = ttk.Spinbox(self.count_spec_position_frame,
+                                               from_=self.MIN_NUMBER_POSITION,
+                                               to=self.MAX_NUMBER_POSITION,
+                                               validate='all',
+                                               validatecommand=(self.register(self._integer_filter), '%P', '%d'))
         self.count_spec_position.bind('<<Increment>>', self._on_increment)
         self.count_spec_position.bind('<<Decrement>>', self._on_decrement)
+        self.count_spec_position.bind('<Return>', self._generate_spec_row)
         self.count_spec_position.set(1)
         self.count_spec_position.grid(row=1, column=1)
 
@@ -42,6 +51,17 @@ class Specification(tk.LabelFrame):
         self.button_frame.grid(sticky="ew")
         # self.save_spec_btn = tk.Button(self.button_frame, text='Експорт специфікації')
         # self.save_spec_btn.pack(side=tk.RIGHT, padx=5)
+
+    def _integer_filter(self, inStr, acttyp):
+        if acttyp == '1':  # insert
+            if not inStr.isdigit():
+                return False
+            if int(inStr) < self.MIN_NUMBER_POSITION:
+                self.count_spec_position.insert(0, self.MIN_NUMBER_POSITION)
+                return False
+            if int(inStr) > self.MAX_NUMBER_POSITION:
+                return False
+        return True
 
     def set_spec_values(self, data):
         num_of_rows = int(len(data) / 6)
@@ -105,8 +125,10 @@ class Specification(tk.LabelFrame):
             self.en3.bind()
             self.en3.grid(row=i + 1, column=2)
             self.en4 = ttk.Entry(self.main_spec_frame, width=5)
+            self.en4.bind("<FocusOut>", lambda event, row_num=i: self._calculate_price(row_num, event))
             self.en4.grid(row=i + 1, column=3)
             self.en5 = ttk.Entry(self.main_spec_frame, width=7)
+            self.en5.bind("<FocusOut>", lambda event, row_num=i: self._calculate_price(row_num, event))
             self.en5.grid(row=i + 1, column=4)
             self.en6 = ttk.Entry(self.main_spec_frame, width=7)
             self.en6.grid(row=i + 1, column=5)
@@ -125,10 +147,34 @@ class Specification(tk.LabelFrame):
                 self.measurement_autocomplete.append(measurement_value)
             entry.configure(completevalues=self.measurement_autocomplete)
 
-    def _on_decrement(self, event):
+    def _on_decrement(self, event=None):
         if len(self.entries) > 6:
             delete_list = self.entries[-1:-7:-1]  # delete row (last 6 Entry)
             for entry in delete_list:
                 entry.destroy()
                 self.entries.pop()
         self.focus()  # remove focus from spinbox
+
+    def _generate_spec_row(self, event=None):
+        new_row_count = int(self.count_spec_position.get())
+        current_row_count = int(len(self.entries) / 6)
+        delta = new_row_count - current_row_count
+
+        if delta > 0:
+            for i in range(delta):
+                self._on_increment()
+        elif delta < 0:
+            for i in range(abs(delta)):
+                self._on_decrement()
+
+    def _calculate_price(self, row_num: int, event):
+        quantity = self.entries[(row_num * 6) - 3].get()
+        price = self.entries[(row_num * 6) - 2].get()
+        if quantity and price:
+            try:
+                total_price = Decimal(price.replace(',', '.')) * Decimal(quantity.replace(',', '.'))
+                round_total_price = total_price.quantize(Decimal("1.00"), ROUND_HALF_UP)
+                self.entries[(row_num * 6) - 1].delete(0, tk.END)
+                self.entries[(row_num * 6) - 1].insert(0, round_total_price)
+            except decimal.InvalidOperation:
+                print("Invalid price")
